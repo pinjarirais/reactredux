64d239a28e4fa9e2f8c16f2a707ecdddc8878dc8
pinjarirais <raj.pinjari@gmail.com>
2024-08-17 21:17:51 +0530

    change routing

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from "react-redux"
 import store from './states/store';
-import { createBrowserRouter, RouterProvider,} from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider,} from "react-router-dom";
 import ErrorPage from './components/ErrorPage';
 import Incdec from './components/Incdec';
 import Home from './components/Home';
@@ -19,59 +19,77 @@ import Hoc from './components/Hoc';
 import UserForm from './components/UserForm';
 import UserFormData from './components/UserFormData';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/getapi",
-        element: <Getapi />,
-      },
-      {
-        path: "/incdec",
-        element: <Incdec />,
-      },
-      {
-        path: "/postapi",
-        element: <Postapi />,
-      },
-      {
-        path: "/deleteapi",
-        element: <DeleteApi />,
-      },
-      {
-        path: "/putapi",
-        element: <PutApi />,
-      },
-      {
-        path: "/memoValue",
-        element: <MemoValue />,
-      },
-      {
-        path: "/liftingup",
-        element: <LiftingUp />,
-      },
-      {
-        path: "/hoc",
-        element: <Hoc />,
-      },
-      {
-        path: "/userform",
-        element: <UserForm />,
-      },
-      {
-        path: "/userformdata",
-        element: <UserFormData />,
-      },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     errorElement: <ErrorPage />,
+//     children: [
+//       {
+//         path: "/",
+//         element: <Home />,
+//       },
+//       {
+//         path: "/getapi",
+//         element: <Getapi />,
+//       },
+//       {
+//         path: "/incdec",
+//         element: <Incdec />,
+//       },
+//       {
+//         path: "/postapi",
+//         element: <Postapi />,
+//       },
+//       {
+//         path: "/deleteapi",
+//         element: <DeleteApi />,
+//       },
+//       {
+//         path: "/putapi",
+//         element: <PutApi />,
+//       },
+//       {
+//         path: "/memoValue",
+//         element: <MemoValue />,
+//       },
+//       {
+//         path: "/liftingup",
+//         element: <LiftingUp />,
+//       },
+//       {
+//         path: "/hoc",
+//         element: <Hoc />,
+//       },
+//       {
+//         path: "/userform",
+//         element: <UserForm />,
+//       },
+//       {
+//         path: "/userformdata",
+//         element: <UserFormData />,
+//       },
+//     ],
+//   },
+// ]);
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />} errorElement={<ErrorPage />} >
+      <Route path='' element={<Home />}/>
+      <Route path='getapi' element={<Getapi />}/>
+      <Route path='incdec' element={<Incdec />}/>
+      <Route path='postapi' element={<Postapi />}/>
+      <Route path='deleteapi' element={<DeleteApi />}/>
+      <Route path='putapi' element={<PutApi />}/>
+      <Route path='memoValue' element={<MemoValue />}/>
+      <Route path='liftingup' element={<LiftingUp />}/>
+      <Route path='hoc' element={<Hoc />}/>
+      <Route path='userform' element={<UserForm />}/>
+      <Route path='userformdata' element={<UserFormData />}/>
+    </Route>
+  )
+);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
